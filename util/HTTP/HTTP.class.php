a799eac7360781dfb12e8df7a7b39ece4fdcb265
liuxd <liuxidong1984@gmail.com>
2013-10-24 16:30:27 +0800

    添加HTTP类。

---
+++ util/HTTP/HTTP.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * HTTP请求相关封装。
+ */
+
+class HTTP {
+    /**
+     * 获得完整HTTP请求头信息。
+     * @return array
+     */
+    public static function getheaders(){
+        $headers = array();
+
+        if (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+            $headers['AUTHORIZATION'] = $_SERVER['PHP_AUTH_DIGEST'];
+        } elseif (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+            $headers['AUTHORIZATION'] = base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $_SERVER['PHP_AUTH_PW']);
+        }
+
+        if (isset($_SERVER['CONTENT_LENGTH'])) {
+            $headers['CONTENT-LENGTH'] = $_SERVER['CONTENT_LENGTH'];
+        }
+
+        if (isset($_SERVER['CONTENT_TYPE'])) {
+            $headers['CONTENT-TYPE'] = $_SERVER['CONTENT_TYPE'];
+        }
+
+        foreach ($_SERVER as $key => $value) {
+            if ('HTTP_' == substr($key, 0, 5)) {
+                $headers[str_replace('_', '-', substr($key, 5))] = $value;
+            }
+        }
+
+        return $headers;
+    }
+}
+
+# end of this file
