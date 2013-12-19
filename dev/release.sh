7f0633da944ba4a81c6d0dd584f1db68d78484c6
liuxd <liuxidong1984@gmail.com>
2013-12-19 23:23:41 +0800

    发布脚本增加语法检查和代码压缩。

--- dev/release.sh
+++ dev/release.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+#检查 & 过滤
+base_str='No syntax errors detected in '
+cur=`pwd`
+cd ../src/
+
+find . -name "*.php" | while read php_file
+do
+    ret=`php -l $php_file`
+    msg="$base_str$php_file"
+
+    if [ "$msg" != $base_str ];then
+        echo $msg
+        git reset --hard
+        exit
+    fi
+
+    php -w $php_file > $php_file
+done
+
+#打包
 cd /Users/liuxd/Documents/github.com/phar-packer
 php phar-packer.php --name=open-sesame --path=/Users/liuxd/Documents/github.com/open-sesame/src --init=index.php
+cd $cur/../src/
+git reset --hard
+
+#部署
 mv open-sesame.phar /Users/liuxd/Documents/web/
