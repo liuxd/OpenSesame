e3f10a7afeab6508ed9d0f275adb8ecfd736b4f1
liuxd <liuxidong1984@gmail.com>
2014-01-24 14:32:43 +0800

    框架需要函数。

---
+++ src/core/Function.php
@@ -0,0 +1,74 @@
+<?php
+
+function see() {
+    $cnt = func_num_args();
+    $values = func_get_args();
+
+    if ($cnt > 1) {
+        foreach ($values as $k => $v) {
+            see($v);
+        }
+
+        return;
+    } else {
+        $value = $values[0];
+    }
+
+    $echo = function ($value, $color, $type) {
+        $len = '';
+
+        if ($type === 'string') {
+            $len = '(' . mb_strlen($value, 'UTF-8') . ')';
+        }
+
+        echo '<font color="',
+        $color,
+        '" style="font-family: arial;word-wrap: break-word;word-break: normal;"><b>',
+        $type,
+        $len,
+        '</b> : ',
+        $value,
+        '</font><br>';
+    };
+
+    switch (TRUE) {
+        case is_string($value) :
+            $echo($value, 'red', 'string');
+            break;
+
+        case is_float($value) :
+            $echo($value, 'BlueViolet', 'float');
+            break;
+
+        case is_int($value) :
+            $echo($value, 'blue', 'int');
+            break;
+
+        case is_null($value) :
+            $echo('null', 'Coral ', 'null');
+            break;
+
+        case is_bool($value) :
+            $v = ($value) ? 'TRUE' : 'FALSE';
+            $echo($v, 'green', 'bool');
+            break;
+
+        case is_array($value) :
+            echo '<b style="font-family:arial">array</b>(', count($value), ')<div style="margin:10px 20px;font-family:arial">';
+
+            foreach ($value as $kk => $vv) {
+                echo '<font color="#555">', $kk, '</font> => ', see($vv);
+            }
+
+            echo '</div>';
+            break;
+
+        default :
+            echo "<pre>";
+            var_export($value);
+            echo "</pre>";
+            break;
+    }
+}
+
+# end of this file
