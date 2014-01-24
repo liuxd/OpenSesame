12cd055250ad90304ec4286bed63c9ab80ef65e6
liuxd <liuxidong1984@gmail.com>
2014-01-24 14:41:13 +0800

    ip()放到Function里。

--- src/core/Function.php
+++ src/core/Function.php
@@ -71,4 +71,24 @@ function see() {
     }
 }
 
+function ip() {
+    if (isset($HTTP_SERVER_VARS["HTTP_X_FORWARDED_FOR"])) {
+        $ip = $HTTP_SERVER_VARS["HTTP_X_FORWARDED_FOR"];
+    } elseif (isset($HTTP_SERVER_VARS["HTTP_CLIENT_IP"])) {
+        $ip = $HTTP_SERVER_VARS["HTTP_CLIENT_IP"];
+    } elseif (isset($HTTP_SERVER_VARS["REMOTE_ADDR"])) {
+        $ip = $HTTP_SERVER_VARS["REMOTE_ADDR"];
+    } elseif (getenv("HTTP_X_FORWARDED_FOR")) {
+        $ip = getenv("HTTP_X_FORWARDED_FOR");
+    } elseif (getenv("HTTP_CLIENT_IP")) {
+        $ip = getenv("HTTP_CLIENT_IP");
+    } elseif (getenv("REMOTE_ADDR")) {
+        $ip = getenv("REMOTE_ADDR");
+    } else {
+        $ip = "Unknown";
+    }
+
+    return $ip;
+}
+
 # end of this file
