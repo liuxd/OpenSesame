a4ef9f940105e9c9f4e2e1fe3ef0ee42eb25f421
liuxd <liuxidong1984@gmail.com>
2014-01-24 16:47:32 +0800

    更新测试用例。

--- tests/OpenSesameTest/util/Gravatar/GravatarTest.php
+++ tests/OpenSesameTest/util/Gravatar/GravatarTest.php
@@ -1,12 +1,12 @@
 <?php
 class GravatarTest extends PHPUnit_Framework_TestCase
 {
-    public function testget_gravatar_url()
+    public function testgetGravatarURL()
     {
         $email = 'liuxidong1984@gmail.com';
         $size = 30;
 
-        $ret = Gravatar::get_gravatar_url($email, $size);
+        $ret = Gravatar::getGravatarURL($email, $size);
         $this->assertEquals('http://www.gravatar.com/avatar/05cdbb54576585408d487d5e8aca534d?s=30&d=&r=G', $ret);
     }
 }
