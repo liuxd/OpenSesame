2e321badd5d4179f39e42df9a5365ce15ec9a1f2
liuxd <liuxidong1984@gmail.com>
2014-01-27 22:20:13 +0800

    PSR

---
+++ src/system/Const.php
@@ -0,0 +1,8 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', __DIR__ . DS . '..' . DS);
+define('UTIL_PATH', ROOT_PATH . 'utility' . DS);
+define('APP_PATH', ROOT_PATH . 'application' . DS);
+define('WWW_PATH', ROOT_PATH . 'public' . DS);
+define('INI_PATH', ROOT_PATH . 'config' . DS);
+define('DEFAULT_APP', 'web');
