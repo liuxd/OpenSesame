17c7acf616256d883db3b8204d15305128682700
liuxd <liuxidong1984@gmail.com>
2014-01-28 14:34:53 +0800

    更新入口文件。

--- src/index.php
+++ src/index.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * 框架入口。
+ * 入口文件
  */
+
 use system as s;
 
 require __DIR__ . '/system/init.inc';
 
-$www = substr($_SERVER['REQUEST_URI'], 0, 8);
-
-if ($www === '/static/') {
+if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/static/') {
     s\FrontEnd::handle(WWW_PATH, 8);
 } else {
     s\CGI::run(s\Router::route());
