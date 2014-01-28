be478061ca255b61cfa3e6d019e4d1372961107c
liuxd <liuxidong1984@gmail.com>
2014-01-28 12:36:24 +0800

    补充文件注释

--- src/system/Const.php
+++ src/system/Const.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * 常量定义
+ */
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT_PATH', __DIR__ . DS . '..' . DS);
 define('UTIL_PATH', ROOT_PATH . 'utility' . DS);
@@ -6,3 +9,5 @@ define('APP_PATH', ROOT_PATH . 'application' . DS);
 define('WWW_PATH', ROOT_PATH . 'public' . DS);
 define('INI_PATH', ROOT_PATH . 'config' . DS);
 define('DEFAULT_APP', 'web');
+
+# end of this file
