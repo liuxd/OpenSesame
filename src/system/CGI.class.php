11658398b2b0ac74bb07f57e057ddd901105695c
liuxd <liuxidong1984@gmail.com>
2014-02-20 09:26:46 +0800

    模板文件名都改为小写

--- src/system/CGI.class.php
+++ src/system/CGI.class.php
@@ -47,7 +47,7 @@ class CGI
         ob_start('ob_gzhandler');
 
         foreach ($o->tpls as $key => $tpl_file) {
-            $real_file = $o->tpl_path . DS . $tpl_file;
+            $real_file = $o->tpl_path . DS . strtolower($tpl_file);
 
             if (is_file($real_file) && file_exists($real_file)) {
                 require $real_file;
