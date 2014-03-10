6751db61003de2a5a3d37e0230d59a7cce480e41
liuxd <liuxidong1984@gmail.com>
2014-03-10 13:53:41 +0800

    使用composer安装phpcs

--- dev/sniffer.sh
+++ dev/sniffer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-phpcs ../src/application --standard=PSR1
-phpcs ../src/system --standard=PSR1
-phpcs ../src/utility --standard=PSR1
-phpcs ../src/index.php --standard=PSR1
 
-phpcs ../src/application --standard=PSR2
-phpcs ../src/system --standard=PSR2
-phpcs ../src/utility --standard=PSR2
-phpcs ../src/index.php --standard=PSR2
+phpcs='vendor/squizlabs/php_codesniffer/scripts/phpcs'
+
+$phpcs ../src/application --standard=PSR1
+$phpcs ../src/system --standard=PSR1
+$phpcs ../src/utility --standard=PSR1
+$phpcs ../src/index.php --standard=PSR1
+
+$phpcs ../src/application --standard=PSR2
+$phpcs ../src/system --standard=PSR2
+$phpcs ../src/utility --standard=PSR2
+$phpcs ../src/index.php --standard=PSR2
