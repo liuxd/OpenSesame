86723359f54151d4c5bcb55648846b0ebaf2ceca
liuxd <liuxidong1984@gmail.com>
2014-03-22 14:30:12 +0800

    重新组织文件。

--- src/index.php
+++ src/index.php
@@ -3,9 +3,9 @@
  * 入口文件
  */
 
-use system as s;
+use core as c;
 
-require __DIR__ . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'init.inc';
+require __DIR__ . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'init.inc';
 
 if (substr($_SERVER['REQUEST_URI'], 0, 8) === '/static/') {
     s\FrontEnd::handle(WWW_PATH, 8);
