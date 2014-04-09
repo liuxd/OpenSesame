805aaeac10c3e7e938ba16987b271af4cce469ba
刘喜东 <liuxidong3@163.com>
2014-04-09 17:39:59 +0800

    优化代码写法

--- src/core/Bootstrap.php
+++ src/core/Bootstrap.php
@@ -8,19 +8,19 @@ date_default_timezone_set('Asia/Shanghai');
 error_reporting(-1);
 
 $aCoreFiles = [
-    'Const.php',
-    'Config.php',
-    'Error.php',
-    'Function.php',
-    'Interface.php',
-    'Loader.php',
-    'Output.php',
-    'Router.php',
-    'Controller.php',
+    'Const',
+    'Config',
+    'Error',
+    'Function',
+    'Interface',
+    'Loader',
+    'Output',
+    'Router',
+    'Controller',
 ];
 
 foreach ($aCoreFiles as $sCoreFile) {
-    require __DIR__ . DIRECTORY_SEPARATOR . $sCoreFile;
+    require __DIR__ . DIRECTORY_SEPARATOR . $sCoreFile . '.php';
 }
 
 require APP_PATH . 'controller/Base.php';
