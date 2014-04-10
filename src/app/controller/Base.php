057829d2194c412c397230df2ebc7449163e9afa
刘喜东 <liuxidong3@163.com>
2014-04-10 13:45:06 +0800

    去掉IP检查和禁止访问页面

--- src/app/controller/Base.php
+++ src/app/controller/Base.php
@@ -12,12 +12,6 @@ class Base extends c\Controller
 
     public function handle()
     {
-        $aIPPower = c\Config::get('ip_check')['data'];
-
-        if ($aIPPower === 'on' && $this->bAuth && !(new m\Secure())->checkIp()) {
-            c\Router::redirect(c\Router::genURL('Deny'));
-        }
-
         $aConfig = c\Config::get('dsn');
         u\DB::getInstance($aConfig['data']);
         $aData = parent::handle();
