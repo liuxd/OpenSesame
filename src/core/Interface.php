9040a2147073d539f134c28193223d146206f997
liuxd <liuxidong1984@gmail.com>
2015-05-20 22:02:42 +0800

    为接口文件添加注释。

--- src/core/Interface.php
+++ src/core/Interface.php
@@ -1,10 +1,25 @@
 <?php
+/**
+ * The Interface for the application controllers.
+ */
+
 namespace core;
 
 interface IController
 {
+    /**
+     * The hook before handler.
+     */
     public function before();
+
+    /**
+     * The main logic method.
+     */
     public function handle();
+
+    /**
+     * The hook after handler.
+     */
     public function after();
 }
 
