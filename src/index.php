ffef96c71d4e0c6e9667629df5e6ccac69886ffa
liuxd <liuxidong1984@gmail.com>
2015-05-20 22:29:01 +0800

    入口文件优化

--- src/index.php
+++ src/index.php
@@ -3,36 +3,24 @@
  * Framework entrance.
  */
 
-// Just for CLI mode.
-if (PHP_SAPI === 'cli') {
-    echo 'Open Sesame 4.3.0', PHP_EOL;
-    echo 'Powered by liuxd', PHP_EOL;
-    echo 'Fork me on github: https://github.com/liuxd', PHP_EOL;
-    die;
-}
+namespace core;
 
-// Initialize the web framework.
 require 'core/Bootstrap.php';
 
-// Response the front end requests.
-\core\Router::responseFrontEndFiles('static');
-
-// Router work, get the target controller object.
-$oController = \core\Router::route($_SERVER['REQUEST_URI'], APP_PATH);
-
-// Run previous hook.
-$oController->before();
-
-// The result data.
-$aData = $oController->handle();
-
-// The request type.Such as, html, json and pjax.
-$sOutputType = $oController->getOutputType();
-
-// Output the result data.
-\core\Output::handle($aData, $sOutputType);
-
-// Run after hook.
-$oController->after();
+// Just for CLI mode.
+if (PHP_SAPI === 'cli') {
+    cecho('Open Sesame 4.3.0');
+    cecho('Powered by liuxd');
+    cecho('Fork me on github: https://github.com/liuxd');
+} else {
+    require APP_PATH . 'controller/Base.php';
+    Router::responseFrontEndFiles('static');
+    $oController = Router::route($_SERVER['REQUEST_URI'], APP_PATH);
+    $oController->before();
+    $aData = $oController->handle();
+    $sOutputType = $oController->getOutputType();
+    Output::handle($aData, $sOutputType);
+    $oController->after();
+}
 
 # end of this file
