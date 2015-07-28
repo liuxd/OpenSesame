86a9d0247a05131b28c7268251abdd660953db43
liuxd <liuxidong1984@gmail.com>
2015-07-28 12:48:49 +0800

    优化代码检查器的输出

--- dev/code-sniffer.sh
+++ dev/code-sniffer.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-cd ../src
+currentPath=`dirname $0`
+cd $currentPath/../
+currentRealPath=`pwd`
 
 # The path of code sniffer.
-phpcs='../dev/vendor/squizlabs/php_codesniffer/scripts/phpcs'
+phpcs=$currentRealPath'/dev/vendor/squizlabs/php_codesniffer/scripts/phpcs'
+cd $currentRealPath/src
+
+phpcsName=`basename $phpcs`
 
 for target in app core util index.php;do
     for mode in PSR1 PSR2;do
-        sniffer="$phpcs -p --colors --tab-width=4 --encoding=utf8 $target --standard=$mode"
+        sniffer="$phpcsName -p --colors --tab-width=4 --encoding=utf8 $target --standard=$mode"
         echo $sniffer
-        $sniffer
+        $phpcs -p --colors --tab-width=4 --encoding=utf8 $target --standard=$mode
     done
 done
 
 echo 'Finished!'
+
+# end of this file
